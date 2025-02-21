8a3bb382ec76195dd3c4b26e717a2956724fada1
Derek Montesdeoca <demont93@gmail.com>
2025-02-21 16:55:14 +0100

    Create partitions.

--- install-debian.bash
+++ install-debian.bash
@@ -12,10 +12,21 @@ apt update && apt upgrade -y
 apt install debootstrap parted lvm2 cryptsetup systemd-timesyncd -y
 
 # ============ Sync Time ============ #
+
 if ! systemctl status systemd-timesyncd; then
     systemctl restart systemd-timesyncd
     systemctl status systemd-timesyncd
 fi
+
+# ============ Make Partitions ============ #
+
+parted -s /dev/sda mklabel msdos
+parted -s /dev/sda mkpart primary 1MiB 501MiB
+parted -s /dev/sda set 1 boot on 
+parted -s /dev/sda mkpart extended 501MiB 100%
+parted -s /dev/sda mkpart logical 501MiB 100%
+parted -s /dev/sda align-check optimal 1
+parted -s /dev/sda align-check optimal 2
 
 # installation_root=debian
 # mkdir -p "$installation_root"
