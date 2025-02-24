2b7c3cf58c2a28cda40b23f634deec2e5c17de4c
Derek Montesdeoca <demont93@gmail.com>
2025-02-24 04:07:02 +0100

    Fixed mount-chroot.

--- mount-chroot.bash
+++ mount-chroot.bash
@@ -3,7 +3,8 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-
+cryptpart="/dev/sda5"
+cryptmapping_name="sda5_crypt"
 if [[ ! -e "/dev/mapper/sda5_crypt" ]]; then
     cryptsetup open --type luks "$cryptpart" "$cryptmapping_name"
 fi
@@ -11,6 +12,7 @@ fi
 installation_root=/mnt
 mkdir -p "$installation_root"
 umount -R -l "$installation_root" || true
+vg="LVMGroup"
 fss=(
     "/dev/mapper/$vg-root"
     "/dev/sda1"
@@ -40,5 +42,22 @@ swap_uuid=$(blkid -s UUID -o value "/dev/mapper/$vg-swap")
 if ! swapon --show=UUID | grep -q "$swap_uuid" ; then
     swapon "/dev/mapper/$vg-swap"
 fi
+
+system_dirs=(
+    "/dev"
+    "/run"
+)
+if ! findmnt "$installation_root/proc"; then
+    mount -t proc "/proc" "$installation_root/proc"
+fi
+if ! findmnt "$installation_root/sys"; then
+    mount -t sysfs "/proc" "$installation_root/sys"
+fi
+for dir in "${system_dirs[@]}"; do
+    if ! findmnt "$installation_root/$dir"; then
+        mount -o X-mount.mkdir --rbind --make-rslave \
+            "$dir" "$installation_root/$dir"
+    fi
+done
 
 chroot "$installation_root" /bin/bash
